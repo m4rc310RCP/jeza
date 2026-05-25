ef000300c3fa33ed0675d201b91c85cc164b4dda
m4rc310 <mlsilva@coamo.com.br>
2026-05-25 13:29:15 -0300

    ...

--- src/v2/@presentation/layouts/geza/screens/switch/switch-screen.v1.tsx
+++ src/v2/@presentation/layouts/geza/screens/switch/switch-screen.v1.tsx
@@ -6,8 +6,7 @@ import {
 } from "@jeza-v2/presentation/layouts";
 import { useLayoutStore } from "@jeza-v2/core/data/zustand/zustand-storage.v1";
 export const MSwithScreen: FC<HtmlHTMLAttributes<unknown>> = () => {
-  const screen = useLayoutStore((s) => s.screen);
-  // const { st_screen } = useMAuth();
+	const screen = useLayoutStore((s) => s.screen);
   return (
     <div className="flex flex-1">
       {screen === "startup" && <MSplasScreenStartup />}
