b53126e11092aa657454d8848eccda7e68f4b60c
m4rc310 <mlsilva@coamo.com.br>
2026-05-25 18:49:49 -0300

    fast versionfunctional version

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,21 @@
 import { createRoot } from "react-dom/client";
 import "leaflet/dist/leaflet.css";
 import "@jeza-v2/presentation/layouts/styles/main.css";
-import { MMainProvider, MAuthProvider } from "@jeza-v2/presentation/contexts";
+import {
+  MMainProvider,
+  MAuthProvider,
+  MAppProvider,
+} from "@jeza-v2/presentation/contexts";
 import { MMainContainer, MSwithScreen } from "@jeza-v2/presentation/layouts";
 
 createRoot(document.getElementById("root")!).render(
   <MMainProvider>
     <MAuthProvider>
-      <MMainContainer>
-        <MSwithScreen />
-      </MMainContainer>
+      <MAppProvider>
+        <MMainContainer>
+          <MSwithScreen />
+        </MMainContainer>
+      </MAppProvider>
     </MAuthProvider>
   </MMainProvider>,
 );
